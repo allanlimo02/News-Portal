61a7265dfa98dfba8c35cc6166de7ebd50786ae2
allanlimo02 <allan.limo@student.moringaschool.com>
2021-09-26 12:13:41 +0300

    Updated database structure

--- src/main/resources/Db/create.sql
+++ src/main/resources/Db/create.sql
@@ -8,7 +8,7 @@ CREATE TABLE IF NOT EXISTS departments  (
     department_name VARCHAR
  );
 
-CREATE TABLE IF NOT EXISTS users (id  int PRIMARY KEY auto_increment, username VARCHAR, department_id INTEGER, role VARCHAR );
+CREATE TABLE IF NOT EXISTS users (id  int PRIMARY KEY auto_increment, username VARCHAR, department_id VARCHAR, role VARCHAR );
 
 CREATE TABLE IF NOT EXISTS news (id  int PRIMARY KEY auto_increment, title VARCHAR, department_id INTEGER, category VARCHAR,news_details VARCHAR);
 
